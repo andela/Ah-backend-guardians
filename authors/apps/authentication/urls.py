36b0871f7699e6a75adc376f7e3146e41381e0a9
Nabulo Vivian Doreen <nabulovivian@MacBook-Pro-73.local>
2019-01-04 12:02:08 +0300

    [delivers #162951656] add swagger documentation

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
 
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
 )
 
+schema_view = get_swagger_view(title='Users API')
+
+
 urlpatterns = [
+    path('swagger-docs/', schema_view),
     path('user/', UserRetrieveUpdateAPIView.as_view(), name='user'),
     path('users/', RegistrationAPIView.as_view(), name='users'),
     path('users/login/', LoginAPIView.as_view(), name='login'),
