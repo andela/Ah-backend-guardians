535bc645bf2474d39e1fe2f59089866514c9ba60
Nabulo Vivian Doreen <nabulovivian@MacBook-Pro-73.local>
2019-01-04 12:41:48 +0300

    document user login using swagger

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
 
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
 )
 
+schema_view = get_swagger_view(title='Login API')
+
 urlpatterns = [
+    url(r'swagger-docs/', schema_view),
     url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
     url(r'^users/?$', RegistrationAPIView.as_view()),
     url(r'^users/login/?$', LoginAPIView.as_view()),
