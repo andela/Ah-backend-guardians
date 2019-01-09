c049bb6642efd8e12a9e7f8d0462cede961df96f
VivianDoreen <nabulovivian2014@gmail.com>
2019-01-09 14:01:57 +0300

    Bug: Implement JWT Authentication - uncomment default_authentication_classes in settings.py - implement JWTAuthentication class
    [maintains #163050194]

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -6,12 +6,42 @@ from rest_framework import authentication, exceptions
 
 from .models import User
 
-"""Configure JWT Here"""
+from django.conf import settings
+
+from rest_framework import authentication, exceptions
+
+from .models import User
 
 
 class JWTAuthentication(authentication.BaseAuthentication):
-    """Class to handle all custom authentication for the application"""
+    authentication_header_prefix = 'Bearer'
 
     def authenticate(self, request):
-        """Method to define custom authentication for requests and return a token"""
-        pass
+        """Authenticate the user """
+        request.user = None
+        # get a list of the items in the header
+        auth_header = authentication.get_authorization_header(request).split()
+        auth_header_prefix = self.authentication_header_prefix.lower()
+
+        if len(auth_header) == 2:
+            prefix = auth_header[0].decode('utf-8')
+            token = auth_header[1].decode('utf-8')
+            if prefix.lower() != auth_header_prefix:
+                msg = 'Bearer is expected as the prefix '
+                raise exceptions.AuthenticationFailed(msg)
+            else:
+                return self._authenticate_credentiatials(request, token)
+        else:
+            return None
+
+    def _authenticate_credentiatials(self, request, token):
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user = User.objects.get(pk=payload['id'])
+            if not user.is_active:
+                msg = 'This user has been deactivated'
+                raise exceptions.AuthenticationFailed(msg)
+            return (user, token)
+        except:
+            msg = 'Invalid authentication. Could not decode token'
+            raise exceptions.AuthenticationFailed(msg)
