42bd544fe40ea38f423bdfb20c91f0d386c5b723
moseskamira <moses.african@gmail.com>
2019-01-10 15:22:46 +0300

    Feature(PasswordReset) Implement a Password Reset functionality
    - write a code that enables a user to reset password and receive a confirmation email
    
    [Finishes #162685661]

--- authors/settings.py
+++ authors/settings.py
@@ -158,3 +158,9 @@ REST_FRAMEWORK = {
     #     'authors.apps.authentication.backends.JWTAuthentication',
     # ),
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'africasfather@gmail.com'
+EMAIL_HOST_PASSWORD = 'kayagajuliet36' # These Shall be put in an environment variable later.
