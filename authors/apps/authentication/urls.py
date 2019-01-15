4b895152b2819c0e0050b09b12d98f041f63e8fb
VivianDoreen <nabulovivian2014@gmail.com>
2019-01-15 10:08:17 +0300

    document api using swagger

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView,
     ResetPasswordAPIView, ResetPasswordConfirmAPIView,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title=" Authors Haven",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -12,4 +28,7 @@ urlpatterns = [
     path('users/login/', LoginAPIView.as_view(), name="login"),
     path('password-reset/', ResetPasswordAPIView.as_view()),
     path('password-reset-confirm/<slug>', ResetPasswordConfirmAPIView.as_view()),
+    path('swagger/', schema_view.with_ui('swagger',
+                                         cache_timeout=0),
+         name='schema-swagger-ui'),
 ]
--- authors/settings.py
+++ authors/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_extensions',
     'rest_framework',
+    'drf_yasg',
 
     'authors.apps.authentication',
     'authors.apps.core',
